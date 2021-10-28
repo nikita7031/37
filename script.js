4211ea75cd2e3e38a9393dc0a255ffeb11572500
nikita7031 <79699356+nikita7031@users.noreply.github.com>
2021-10-28 18:56:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var roll = new Audio('roulette_spin.wav');
+const b_color=document.querySelector('#number_box');
+// let document.getElementById('number_background');
+document.getElementById('spin').addEventListener("click", spin);
+
+function spin() {
+    console.log('spin');
+    roll.play()
+    for (i=0; i<141; i++){
+        // Runs 25 times, with step from 0 to 140
+        // Returns a random integer from 0 to 37
+        (function(i){
+            window.setTimeout(function(){
+                rand=Math.floor(Math.random()*38)
+                // rand=Math.floor(Math.random()*3)
+                document.getElementById('number').innerHTML=rand
+                if (rand%2==0) {
+                    b_color.style.backgroundColor='#202020';
+                }
+                if(rand%2!=0){
+                    b_color.style.backgroundColor='#a52a2a';
+                }
+                if(rand==0){
+                    b_color.style.backgroundColor='#0a8f0a';
+                }
+            },i*50);
+        }(i));
+    };
+}
